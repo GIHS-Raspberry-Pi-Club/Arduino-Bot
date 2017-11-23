3ef98a0264f6f49883cd6b5d45a57135ed4bf8c0
archergs <30485025+archergs@users.noreply.github.com>
2017-11-23 14:05:47 +1030

    runaway_bot

---
+++ runaway_bot.ino
@@ -0,0 +1,52 @@
+const int motor_LF = 2;
+const int motor_LB = 3;
+const int motor_RB = 4;
+const int motor_RF = 5;
+
+const int trig_pin = 9;
+const int echo_pin = 10;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(motor_LF, OUTPUT);
+  pinMode(motor_LB, OUTPUT);
+  pinMode(motor_RF, OUTPUT);
+  pinMode(motor_RB, OUTPUT);
+
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echo_pin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  
+  if (distance <= 10) {
+      digitalWrite(motor_LF, HIGH);
+      digitalWrite(motor_RB, HIGH);
+      delay(3000);
+      reset_motors();
+  } else {
+    reset_motors();
+    digitalWrite(motor_LF, HIGH);
+    digitalWrite(motor_RF, HIGH);
+  }
+}
+
+void reset_motors() {
+  digitalWrite(motor_LF, LOW);
+  digitalWrite(motor_LB, LOW);
+  digitalWrite(motor_RF, LOW);
+  digitalWrite(motor_RB, LOW);
+}
